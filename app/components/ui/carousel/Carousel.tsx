d71d157c7a93085b4209f35a641980e226faa594
ifeoma-joy-obasi <ifeomaobasi1999@gmail.com>
2025-09-27 05:12:28 +0100

    fixed the carousel dots on smaller screens

--- app/components/ui/carousel/Carousel.tsx
+++ app/components/ui/carousel/Carousel.tsx
@@ -137,7 +137,7 @@ const prev = () => {
       </div>
 
       {/* dots */}
-      <div className="mt-3 flex gap-2 absolute left-13  md:left-75 lg:left-110 xl:left-160 ">
+      <div className="mt-3 flex gap-2 absolute left-12  md:left-75 lg:left-110 xl:left-160 ">
         {IMAGES.map((_, idx) => (
           <button
             key={idx}
